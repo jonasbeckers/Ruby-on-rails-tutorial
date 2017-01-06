fe6dbc4be329c8ff33b5cd4aca91737604c88a82
jonasbeckers <jonas.beckers1996@gmail.com>
2017-01-06 19:34:44 +0100

    finished chapter 7

--- sample_app/app/helpers/users_helper.rb
+++ sample_app/app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+
 end
